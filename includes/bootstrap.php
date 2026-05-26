834e84df9c4217995b785559bb2a310c807b55e4
karadag <nissa.karadag@ynov.com>
2026-05-26 14:34:35 +0200

    docker

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -2,8 +2,10 @@
 declare(strict_types=1);
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
-$dotenv->load();
+if (file_exists(__DIR__ . '/../.env')) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+    $dotenv->load();
+}
 
 $secure = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off');
 session_set_cookie_params([
